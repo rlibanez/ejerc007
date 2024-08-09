134cb1bde99509b7a2c9ad76a5daee111b37eb16
curso09 <curso09@nomail.com>
2024-08-09 10:35:41 +0200

    Proyecto inicial vacío

---
+++ src/main/java/es/cic/curso/ejerc007/Ejerc007Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso.ejerc007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerc007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerc007Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso/ejerc007/Ejerc007ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso.ejerc007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerc007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
